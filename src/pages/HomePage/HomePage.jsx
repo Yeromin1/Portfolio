3b028b191faf992c3679f7d6f008d32bbacc2ecc
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-05-05 16:05:40 +0300

    homeTest2

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,8 +3,8 @@ import myPhoto from "../../assets/images/MyFoto.jpg";
 
 const HomePage = () => {
   return (
-    <section id="home" className="bg-[#020e26] py-20">
-      <div className="container grid gap-[50px]">
+    <section id="home" className="h-screen bg-[#020e26] pt-[110px] pb-[80px]">
+      <div className="container grid gap-[60px]">
         <div className="bg-white/5 border-2 border-cyan-400/20 rounded-[20px] p-10 shadow-[0_0_25px_rgba(0,255,255,0.08)]">
           {/* TITLE ANIMATION */}
           <motion.h1
@@ -71,7 +71,7 @@ const HomePage = () => {
           transition={{ delay: 1, duration: 0.6 }}
           className="avatar mx-auto"
         >
-          <div className="ring-4 ring-[#094165] ring-offset-base-100 w-32 rounded-full ring-2 shadow-[0_0_25px_#fff,_0_0_40px_rgba(0,191,255,0.4)]">
+          <div className="ring-4 ring-[#094165] ring-offset-base-100 w-44 rounded-full ring-2 shadow-[0_0_25px_#fff,_0_0_40px_rgba(0,191,255,0.4)]">
             <img src={myPhoto} alt="My photo" />
           </div>
         </motion.div>
