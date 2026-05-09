d0854a50fbbe0ef0c184f0720e2389af2ed67ae3
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-05-09 16:38:10 +0300

    motion

--- src/animations/reveal/Motion.jsx
+++ src/animations/reveal/Motion.jsx
@@ -1,27 +1,3 @@
-// import { motion } from "framer-motion";
-// import { fadeUp } from "./variants";
-
-// export const Motion = ({
-//   children,
-//   variant = fadeUp,
-//   delay = 0,
-//   duration = 0.6,
-//   className = "",
-// }) => {
-//   return (
-//     <motion.div
-//       variants={variant}
-//       initial="hidden"
-//       whileInView="visible"
-//       viewport={{ once: true }}
-//       transition={{ duration, delay }}
-//       className={className}
-//     >
-//       {children}
-//     </motion.div>
-//   );
-// };
-
 import { motion } from "framer-motion";
 import { fadeUp } from "./variants";
 
@@ -36,7 +12,8 @@ export const Motion = ({
     <motion.div
       variants={variant}
       initial="hidden"
-      animate="visible"
+      whileInView="visible"
+      viewport={{ once: true }}
       transition={{ duration, delay }}
       className={className}
     >
