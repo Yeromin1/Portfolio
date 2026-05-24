f4609109010d7c70687c77e5abf385c0fabdd1a9
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-05-24 22:45:25 +0300

    SplashScreen-css

--- src/components/SplashScreen/SplashScreen.jsx
+++ src/components/SplashScreen/SplashScreen.jsx
@@ -1,9 +1,12 @@
-import Portfolio from "../../assets/images/Portfolio.png";
-
 const SplashScreen = () => {
   return (
-    <div className="fixed inset-0 z-[999] flex items-center justify-center bg-[#030A14]">
-      <img src={Portfolio} alt="Portfolio" className="" />
+    <div className="h-screen flex flex-col z-[999] gap-16 items-center justify-center bg-[#0a192f] shadow-[0_0_30px_cyan,_0_0_60px_cyan_inset]">
+      <h2 className="text-5xl text-[#0d1111] text-shadow-[0_0_5px_#00ffff,_0_0_10px_#00ffff,_0_0_20px_#00ffff,_0_0_40px_#00ffff]">
+        Welcome to my
+      </h2>
+      <h3 className="text-7xl text-[#0d1111] text-shadow-[0_0_5px_#00ffff,_0_0_10px_#00ffff,_0_0_20px_#00ffff,_0_0_40px_#00ffff]">
+        Portfolio
+      </h3>
     </div>
   );
 };
