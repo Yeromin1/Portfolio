e261ef962954620ff9c4c6e3bbe99d3f16c4377d
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2026-06-03 20:49:40 +0300

    retro-Contact

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -13,15 +13,15 @@ const ContactPage = () => {
     <section id="contact" className="bg">
       <div className="container py-20 md:py-28">
         <Motion delay={0.1}>
-          <div className="bg-[rgba(255,255,255,0.03)] border border-[rgba(0,255,255,0.2)] rounded-[20px] shadow-[0_0_30px_rgba(0,255,255,0.08)] py-[60px] px-10 md:w-[90%] md:mx-auto">
+          <div className="bg-[var(--bg-card-contact)] border border-[var(--border-btn-header)] rounded-[20px] shadow-[var(--box-shadow-card-contact)] py-[60px] px-10 md:w-[90%] md:mx-auto">
             <Motion delay={0.1}>
-              <h2 className=" text-center text-[42px] font-bold text-[#00ffff] mb-4 tracking-[1px]">
+              <h2 className=" text-center text-[42px] font-bold text-[var(--text-position)] mb-4 tracking-[1px]">
                 {t("title")}
               </h2>
             </Motion>
 
             <Motion delay={0.3}>
-              <p className="text-[#b8c2cc] leading-relaxed mb-9 text-center">
+              <p className="text-[var(--text-contact)] leading-relaxed mb-9 text-center">
                 {t("description")}
               </p>
             </Motion>
@@ -29,7 +29,7 @@ const ContactPage = () => {
             <div className="flex gap-4 justify-center mb-6 md:gap-6 md:mb-8 lg:gap-8 lg:mb-10">
               <Motion delay={0.4}>
                 <a
-                  className="w-[60px] h-[60px] bg-[rgba(0,255,255,0.06)] border border-[rgba(0,255,255,0.3)] rounded-[15px] flex justify-center items-center transition duration-300 ease-in-out backdrop-blur-[10px] md:hover:scale-115 md:hover:shadow-[0_0_10px_#64ffda,_0_0_20px_#00bfff]"
+                  className="w-[60px] h-[60px] bg-[var(--bg-contact-network)] border border-[var(--border-btn-projects)] rounded-[15px] flex justify-center items-center transition duration-300 ease-in-out backdrop-blur-[10px] md:hover:scale-115 cart-shadow-hover"
                   href="http://www.linkedin.com/in/dmytro-yeromin-2a2933338"
                   target="_blank"
                   rel="noreferrer"
@@ -46,7 +46,7 @@ const ContactPage = () => {
 
               <Motion delay={0.5}>
                 <a
-                  className="w-[60px] h-[60px] bg-[rgba(0,255,255,0.06)] border border-[rgba(0,255,255,0.3)] rounded-[15px] flex justify-center items-center transition duration-300 ease-in-out backdrop-blur-[10px] md:hover:scale-115 md:hover:shadow-[0_0_10px_#64ffda,_0_0_20px_#00bfff]"
+                  className="w-[60px] h-[60px] bg-[var(--bg-contact-network)] border border-[var(--border-btn-projects)] rounded-[15px] flex justify-center items-center transition duration-300 ease-in-out backdrop-blur-[10px] md:hover:scale-115 cart-shadow-hover"
                   href="http://t.me/dmytro_yeromin"
                   target="_blank"
                   rel="noreferrer"
@@ -61,7 +61,7 @@ const ContactPage = () => {
 
               <Motion delay={0.6}>
                 <a
-                  className="w-[60px] h-[60px] bg-[rgba(0,255,255,0.06)] border border-[rgba(0,255,255,0.3)] rounded-[15px] flex justify-center items-center transition duration-300 ease-in-out backdrop-blur-[10px] md:hover:scale-115 md:hover:shadow-[0_0_10px_#64ffda,_0_0_20px_#00bfff]"
+                  className="w-[60px] h-[60px] bg-[var(--bg-contact-network)] border border-[var(--border-btn-projects)] rounded-[15px] flex justify-center items-center transition duration-300 ease-in-out backdrop-blur-[10px] md:hover:scale-115 cart-shadow-hover"
                   href="https://github.com/Yeromin1"
                   target="_blank"
                   rel="noreferrer"
@@ -78,7 +78,7 @@ const ContactPage = () => {
             <Motion delay={0.7}>
               <div className="flex justify-center">
                 <a
-                  className="px-[35px] py-[14px] bg-[linear-gradient(135deg,_#00ffff,_#00c2ff)] text-[#030A14] font-semibold rounded-[30px] transition-all duration-300 ease-in-out shadow-[0_0_20px_rgba(0,255,255,0.4)] flex items-center md:hover:scale-115 md:hover:shadow-[0_0_20px_#64ffda,_0_0_35px_#00bfff]"
+                  className="px-[35px] py-[14px] [background:var(--bg-contact-link)] text-[#030A14] font-semibold rounded-[30px] transition-all duration-300 ease-in-out shadow-[var(--box-shadow-link-contact)] flex items-center md:hover:scale-115 shadow-contact-link"
                   href="mailto:yeromin.dmitriy@gmail.com"
                   target="_blank"
                   rel="noreferrer"
